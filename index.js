62713dfd4b276c146f595eeccf273293de849fc7
Natnael Alemayehu <se.natnael.alemayehu@gmail.com>
2024-07-06 11:42:04 +0300

    Exercise 3.2 completed

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+    const length = persons.length
+    const time = Date()
+    res.send(`<p>Phonebook has info for ${length} people</p>  <p>${time}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log("Server running on", PORT);
